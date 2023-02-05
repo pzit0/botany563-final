a0e8eab53e645891eb50c3cd777d05e3264cd0be
pzit0 <78287363+pzit0@users.noreply.github.com>
2023-02-05 16:30:03 -0600

    xml is not working, will try to do it through string and manually construct dict

--- data-collection.py
+++ data-collection.py
@@ -3,8 +3,27 @@ Patricia Zito
 Feb 5, 2022
 '''
 
+import xmltodict
 import Bio 
 from Bio import Entrez 
 Entrez.email = "pzito@wisc.edu"
 
+acc = "TRY75264.1"
 # getting info from accession number 
+handle = Entrez.efetch(db = "protein", id = acc, rettype = "gb", retmode = "xml")
+info = handle.read()
+handle.close()
+dic = xmltodict.parse(info)
+
+hand = Entrez.efetch(db = "protein", id = acc, rettype = "gb", retmode = "text")
+readable = hand.read()
+read_split = readable.split()
+keys = []
+values = []
+for i in range(len(read_split)):
+    print(read_split[i])
+    if (i%2) == 0:
+        keys += read_split[i]
+    else:
+        values += read_split[i]
+
