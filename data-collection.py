f3e25f36227f7e5acd4d7d96279ffcc2fb62bbef
pzit0 <78287363+pzit0@users.noreply.github.com>
2023-02-05 18:15:23 -0600

    efetch has stopped magically stopped working. progress on parsing, but still non-functional

--- data-collection.py
+++ data-collection.py
@@ -3,7 +3,7 @@ Patricia Zito
 Feb 5, 2022
 '''
 
-import xmltodict
+
 import Bio 
 from Bio import Entrez 
 Entrez.email = "pzito@wisc.edu"
@@ -12,38 +12,37 @@ acc = "TRY75264.1" # for testing
 
 # getting info from accession number 
 
-# acquiring information 
+# acquiring handle  
 handle = Entrez.efetch(db = "protein", id = acc, rettype = "gb", retmode = "text")
 readable = handle.read()
 handle.close()
 
 # parsing and putting info into dict 
+# I've tried through xmltodict and it didn't work
+# have to do it manually 
 read_split = readable.split()
-read_split
+
 keys = ['LOCUS', 'DEFINITION', 'ACCESSION', 'VERSION', 'DBLINK', 'KEYWORDS', 'SOURCE',
     'ORGANISM', 'REFERENCE', 'AUTHORS', 'TITLE', 'JOURNAL', 'PUBMED', 'REFERENCE', 'AUTHORS'
     'TITLE', 'JOURNAL', 'COMMENT', 'FEATURES', 'ORIGIN']
-copy_keys = keys
-index_keys = [] # this contains index for where each key is in read_split
-values = []
+
 my_dict = {}
+key = "start" # temp
+values = [] # temp 
 
 for i in range(len(read_split)):
-    if read_split[i] == keys[0]:
-        index_keys.append(i)
-        keys.remove(keys[0])
+    if read_split[i] == keys[0]: # if this is a key 
+        if values: # if value is full 
+            my_dict[key] = values # match key and value 
+            print("MATCHED!!!!!!", key, my_dict[key])
+            key = keys[0] # current key 
+            keys.remove(keys[0]) # start looking for next boundary
+            values = [] # restart values 
+        else: # value is empty
+            key = keys[0] # only take key
+    else: # if this is a value 
+        values.append(read_split[i]) # add it to values 
 
-for i in range(len(index_keys)): 
-    print(index_keys[i])
-    print(read_split[index_keys[i+1]:index_keys[i+1]-1])
-    my_dict[index_keys[i]] = read_split[index_keys[i+1]:index_keys[i+1]-1]
-        
-for i in range(len(read_split)):
-    if read_split[i] == keys[0]: # if this is the right word
-        my_dict[keys[0]] = "empty" # make it a dict key
-        keys.remove(keys[0]) # remove from options
-    else: # if not, it is a value 
-        values.append(read_split[i]) # add it to temp value 
-        my_dict[copy_keys]
+
 
 
